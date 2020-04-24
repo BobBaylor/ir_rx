7e20e8803c712129a8362b71d9608c7f9d9b533f
Bob Baylor <SkiBoyBob@gmail.com>
2020-04-24 13:07:55 -0700

    comments

--- ir_rx.py
+++ ir_rx.py
@@ -1,6 +1,13 @@
 # ir_rx.py
-# receive IR remote transmisions and operate a volume control
+# receive IR remote transmisions and (eventually) operate a volume control
+"""Notes:
+    I've flipped the mar-space terminology bc the bit information is actually in the spaces
+    and that seems backwards to me.
 
+    We use the pigpio callback and timeout features to capture tranmissions.
+
+    Time is in us and frequencey in MHz to make easy conversions.
+"""
 
 import time
 
@@ -27,50 +34,75 @@ usage_text = """
 
 
 class IrReceiver():
+    """ A class to encapsulate the reception and decoding process.
+        Init with a dict of all the options when using it stand-alone.
+            opts = {'--glitch': 100,
+                    '--pin': 3,
+                    '--pre': 50,
+                    '--file': '',
+                    '--post': 15,
+                    '--raw': '',
+                    '--short': 2,
+                    '--tolerance': 15,
+                    '--verbose': False,
+                   }
+    """
     def __init__(self, opts):
         self.opts = opts
         self.pin_ir = int(opts['--pin'])
-        self.pig = pigpio.pi()
-        if self.opts['--verbose']:
-            hdw_ver = self.pig.get_hardware_revision()
-            print('Found hardware version %06x and using GPIO%02d'%(hdw_ver, self.pin_ir))
-
-        self.pig.set_mode(self.pin_ir, pigpio.INPUT)
-        self.pig.set_glitch_filter(self.pin_ir, int(opts['--glitch'])) # Ignore glitches.
-        self.pig.set_pull_up_down(self.pin_ir, pigpio.PUD_UP)
-        self.carrier_MHz = 0.04         # 40 kHz - sb a CL arg
-        self.last_tick = 0
-        self.in_code = False
-        self.code = []
-        self.codes = []
-        self.fetching_code = False
         self.pre_us = int(opts['--pre']) * 1000
         self.post_ms = int(opts['--post'])
         self.tolerance_pct = int(opts['--tolerance'])
         self.glitch_us = int(opts['--glitch'])
         self.short = int(opts["--short"])
-        self.last_code = None
+
+        self.pig = pigpio.pi()  # open the pi gpio
+
+        if self.opts['--verbose']:
+            hdw_ver = self.pig.get_hardware_revision()
+            print('Found hardware version %06x and using GPIO%02d'%(hdw_ver, self.pin_ir))
+
+        # setup the pin, the glitch filter, and a pull on the pin
+        self.pig.set_mode(self.pin_ir, pigpio.INPUT)
+        self.pig.set_glitch_filter(self.pin_ir, int(opts['--glitch'])) # Ignore glitches.
+        self.pig.set_pull_up_down(self.pin_ir, pigpio.PUD_UP)
+
+        self.carrier_MHz = 0.04         # 40 kHz - sb a CL arg I guess
+        self.last_tick = 0
+        self.in_code = False
+        self.events = []        # the tranmission we're currently building.
+        self.codes = []         # all the transmissions we're storing
+        self.look_for_a_code = False   # tell the instance to watch the IR. Cleared when one found
+        self.last_code = None   # store the last code for use with "repeat" transmission(s)
 
     def end_of_code(self):
-        if len(self.code) > self.short:
-            # normalise(code)
-            self.fetching_code = False
-            self.codes.append([e[1] for e in self.code])
+        """ We think we've captured a code.
+        """
+        if len(self.events) > self.short:
+            # normalise(events)
+            self.look_for_a_code = False
+            self.codes.append([e[1] for e in self.events])
             if self.opts['--verbose']:
-                print('Code Found',self.code[0][0])
-            # print('Code Found:','\n'.join(['%r:%5d'%e for e in self.code]))
+                print('Code Found',self.events[0][0])
+            # print('Code Found:','\n'.join(['%r:%5d'%e for e in self.events]))
         else:
             if self.opts['--verbose']:
-                print("Short code, probably a repeat, try again")
-        self.code = []
+                print("Short code <", self.short)
+        self.events = []
 
 
     def cbf(self, gpio, level, tick):
-        if level != pigpio.TIMEOUT:
+        """ The callback function get called once for each event (edge) detected by the daemon.
+            gpio is pi GPIO number (which we don't need).
+            level is 1 or 0 indicating the state of the pin *after* the edge
+            tick is a timer value (default resolution is 5 us) we use to determine length
+                of the burst, or space between bursts.
+        """
+        if level != pigpio.TIMEOUT:    # here's an edge
             edge = pigpio.tickDiff(self.last_tick, tick)
             self.last_tick = tick
 
-            if self.fetching_code:
+            if self.look_for_a_code:
                 if (edge > self.pre_us) and (not self.in_code): # Start of a code.
                     self.in_code = True
                     self.pig.set_watchdog(self.pin_ir, self.post_ms) # Start watchdog.
@@ -82,15 +114,20 @@ class IrReceiver():
 
                 elif self.in_code:
                     # flip polarity bc hardware low means burst detected
-                    self.code.append((0 if level else 1, edge),)
+                    self.events.append((0 if level else 1, edge),)
 
-        else:
+        else:   # timeout. Perhaps we have a code to store
             self.pig.set_watchdog(self.pin_ir, 0) # Cancel watchdog.
             if self.in_code:
                 self.in_code = False
                 self.end_of_code()
 
+
     def compare(self, observed, expected):
+        """ Handy function to compare an observed list with a test list
+            including a tolerance for error in the timing.
+            Returns True if observed matches expected within tolerance.
+        """
         for pair in zip(observed, expected):
             if pair[0] < pair[1] * (1.0 - self.tolerance_pct*0.01):
                 return False
@@ -98,33 +135,39 @@ class IrReceiver():
                 return False
         return True
 
+
     def close(self):
+        # cleanup
         self.pig.stop() # Disconnect from Pi.
 
+
     def to_cycles(self, a_code):
-        # a_code is a list, in us
-        # so cycles is val_us/period_us or val_us*freq_MHz
-        # returns list of floats
+        """ convert code event timings to floats of the cycle counts.
+        """
         return [self.carrier_MHz * event for event in a_code]
 
+
     def is_repeat(self, cycles):
-        # a_code is in float cycles
+        # detect the nec 'repeat' transmission
         return self.compare(cycles, [360, 90, 22.5])
 
 
     def has_preample(self, cycles):
-        # a_code is in float cycles
+        # detect the nec preamble
         return self.compare(cycles, [360, 180,])
 
 
     def str_cycles(self, cycles):
-        # cycles is in float cycles
+        # format length: x0 x1 x2 ...
         m_str =  '%d: '%(len(cycles),) + ' '.join(['%2.0f'%round(c) for c in cycles])
         return m_str
 
 
     def one_mark(self, mark):
-        # return a '1', a '0', or an 'x' based on the mark (float cycles)
+        """ The nec marks and spaces are defined by their cycle count.
+            Return a '1', '0', (or 'x' if it matches neither).
+            Used by decode_nec() to make a binary repr of the data
+        """
         if self.compare([mark], [21.0]):
             return '0'
         if self.compare([mark], [66.0]):
@@ -133,12 +176,16 @@ class IrReceiver():
 
 
     def decode_nec(self, marks):
-        # marks is in float cycles. Preamble and spaces have been stripped
+        """ Preamble and spaces have been stripped before calling decode_nec().
+            Check that all the remaining events are either a 0 or 1
+            Convert to bytes and perform the inversion check
+            Return the address and command bytes along with a bool indicating valid
+        """
         b_str =  ''.join([self.one_mark(c) for c in marks])[::-1]  # binary, reversed
         if self.opts['--verbose']:
             print('b_str', b_str)
         if 'x' in b_str:
-            address, command, b_ok = 0, 0, False    # any 'x' was outside of tol
+            address, command, b_ok = 0, 0, False    # any 'x' was outside of tolerance
         else:
             # separate into 8 bit blocks and convert to int and reverse again
             i_values = [int(b_str[i:i+8], 2) for i in range(0, 32, 8)][::-1]
@@ -153,7 +200,9 @@ class IrReceiver():
 
 
     def show_code(self, a_code):
-        """ possibilties:
+        """ For testing. Use get_commands() to get any new transmissions.
+
+            Print the varous possibilties:
                 valid preamble and a code
                 valid preamble but garbled code  (inversions don't match)
                 valid repeat
@@ -188,7 +237,10 @@ class IrReceiver():
 
 
     def get_commands(self):
-        # generator to return each valid decoded code, including repeats. consumes the codes
+        """ generator to return each valid decoded code,
+            'repeat' code returns a copy of the previous code.
+            The function consumes the codes list.
+        """
         codes_cpy = self.codes[::-1]
         self.codes = []
         while True:
@@ -210,31 +262,35 @@ class IrReceiver():
 
 
 def test(opts):
+    """ Test the IrReceiver class.
+        opts is a dict of command line options
+    """
     rcvr = IrReceiver(opts)
+    # install the callback
     cb_func = rcvr.pig.callback(rcvr.pin_ir, pigpio.EITHER_EDGE, rcvr.cbf)
-    assert cb_func
+    assert cb_func        # the daemon might not be running
 
     t_start = time.time()
-    done = False
+    done = False        # loop until our 10 seconds elapses
     while not done:
-        rcvr.fetching_code = True
-        while rcvr.fetching_code:
+        # each time we rx a complete code, we stop looking
+        rcvr.look_for_a_code = True     # keep telling the IrReceiver to look
+
+        while rcvr.look_for_a_code:
             for a_cmd in rcvr.get_commands():
-                print(a_cmd)
-            if time.time() > t_start + 10:
+                print(a_cmd)            # tuple of (address, data)
+
+            if time.time() > t_start + 10:  # don't run forever
                 rcvr.close()
                 done = True
                 break
             time.sleep(0.1)
 
+    rcvr.pig.stop() # Disconnect from Pi.
+
     # show what we captured - should be empty bc get_commands() drains it
     for a_code in rcvr.codes:
         rcvr.show_code(a_code)
-
-    rcvr.pig.stop() # Disconnect from Pi.
-
-
-
 
 
 if __name__ == '__main__':
